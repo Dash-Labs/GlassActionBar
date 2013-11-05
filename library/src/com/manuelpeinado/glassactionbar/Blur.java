bfbf6b371e7faafd331ac5a61b8402def072917e
Chen Yuan <chen@dash.by>
2013-11-05 15:12:10 -0500

    Costumized action bar for transparent  and blur effect
    Modified for list view, when list header is in screen, action bar is
    transparent. When user scrolls list header out of screen, action bar
    will become blur.

--- library/src/com/manuelpeinado/glassactionbar/Blur.java
+++ library/src/com/manuelpeinado/glassactionbar/Blur.java
@@ -3,37 +3,17 @@ package com.manuelpeinado.glassactionbar;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.os.Build.VERSION;
-import android.renderscript.Allocation;
-import android.renderscript.Element;
-import android.renderscript.RenderScript;
-import android.renderscript.ScriptIntrinsicBlur;
 
 // Code borrowed from Nicolas Pomepuy
 // https://github.com/PomepuyN/BlurEffectForAndroidDesign
 public class Blur {
-
+    
     public static Bitmap apply(Context context, Bitmap sentBitmap) {
         return apply(context, sentBitmap, GlassActionBar.DEFAULT_BLUR_RADIUS);
     }
-
+    
     @SuppressLint("NewApi")
     public static Bitmap apply(Context context, Bitmap sentBitmap, int radius) {
-        if (VERSION.SDK_INT > 16) {
-            Bitmap bitmap = sentBitmap.copy(sentBitmap.getConfig(), true);
-
-            final RenderScript rs = RenderScript.create(context);
-            final Allocation input = Allocation.createFromBitmap(rs, sentBitmap, Allocation.MipmapControl.MIPMAP_NONE,
-                    Allocation.USAGE_SCRIPT);
-            final Allocation output = Allocation.createTyped(rs, input.getType());
-            final ScriptIntrinsicBlur script = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
-            script.setRadius(radius);
-            script.setInput(input);
-            script.forEach(output);
-            output.copyTo(bitmap);
-            return bitmap;
-        }
-
         // Stack Blur v1.0 from
         // http://www.quasimondo.com/StackBlurForCanvas/StackBlurDemo.html
         //
@@ -43,7 +23,7 @@ public class Blur {
         // Android port : Yahel Bouaziz <yahel at kayenko.com>
         // http://www.kayenko.com
         // ported april 5th, 2012
-
+        
         // This is a compromise between Gaussian Blur and Box blur
         // It creates much better looking blurs than Box Blur, but is
         // 7x faster than my Gaussian Blur implementation.
@@ -61,39 +41,39 @@ public class Blur {
         // the following line:
         //
         // Stack Blur Algorithm by Mario Klingemann <mario@quasimondo.com>
-
+        
         Bitmap bitmap = sentBitmap.copy(sentBitmap.getConfig(), true);
-
+        
         if (radius < 1) {
             return (null);
         }
-
+        
         int w = bitmap.getWidth();
         int h = bitmap.getHeight();
-
+        
         int[] pix = new int[w * h];
         bitmap.getPixels(pix, 0, w, 0, 0, w, h);
-
+        
         int wm = w - 1;
         int hm = h - 1;
         int wh = w * h;
         int div = radius + radius + 1;
-
+        
         int r[] = new int[wh];
         int g[] = new int[wh];
         int b[] = new int[wh];
         int rsum, gsum, bsum, x, y, i, p, yp, yi, yw;
         int vmin[] = new int[Math.max(w, h)];
-
+        
         int divsum = (div + 1) >> 1;
         divsum *= divsum;
         int dv[] = new int[256 * divsum];
         for (i = 0; i < 256 * divsum; i++) {
             dv[i] = (i / divsum);
         }
-
+        
         yw = yi = 0;
-
+        
         int[][] stack = new int[div][3];
         int stackpointer;
         int stackstart;
@@ -102,7 +82,7 @@ public class Blur {
         int r1 = radius + 1;
         int routsum, goutsum, boutsum;
         int rinsum, ginsum, binsum;
-
+        
         for (y = 0; y < h; y++) {
             rinsum = ginsum = binsum = routsum = goutsum = boutsum = rsum = gsum = bsum = 0;
             for (i = -radius; i <= radius; i++) {
@@ -126,52 +106,52 @@ public class Blur {
                 }
             }
             stackpointer = radius;
-
+            
             for (x = 0; x < w; x++) {
-
+                
                 r[yi] = dv[rsum];
                 g[yi] = dv[gsum];
                 b[yi] = dv[bsum];
-
+                
                 rsum -= routsum;
                 gsum -= goutsum;
                 bsum -= boutsum;
-
+                
                 stackstart = stackpointer - radius + div;
                 sir = stack[stackstart % div];
-
+                
                 routsum -= sir[0];
                 goutsum -= sir[1];
                 boutsum -= sir[2];
-
+                
                 if (y == 0) {
                     vmin[x] = Math.min(x + radius + 1, wm);
                 }
                 p = pix[yw + vmin[x]];
-
+                
                 sir[0] = (p & 0xff0000) >> 16;
                 sir[1] = (p & 0x00ff00) >> 8;
                 sir[2] = (p & 0x0000ff);
-
+                
                 rinsum += sir[0];
                 ginsum += sir[1];
                 binsum += sir[2];
-
+                
                 rsum += rinsum;
                 gsum += ginsum;
                 bsum += binsum;
-
+                
                 stackpointer = (stackpointer + 1) % div;
                 sir = stack[(stackpointer) % div];
-
+                
                 routsum += sir[0];
                 goutsum += sir[1];
                 boutsum += sir[2];
-
+                
                 rinsum -= sir[0];
                 ginsum -= sir[1];
                 binsum -= sir[2];
-
+                
                 yi++;
             }
             yw += w;
@@ -181,19 +161,19 @@ public class Blur {
             yp = -radius * w;
             for (i = -radius; i <= radius; i++) {
                 yi = Math.max(0, yp) + x;
-
+                
                 sir = stack[i + radius];
-
+                
                 sir[0] = r[yi];
                 sir[1] = g[yi];
                 sir[2] = b[yi];
-
+                
                 rbs = r1 - Math.abs(i);
-
+                
                 rsum += r[yi] * rbs;
                 gsum += g[yi] * rbs;
                 bsum += b[yi] * rbs;
-
+                
                 if (i > 0) {
                     rinsum += sir[0];
                     ginsum += sir[1];
@@ -203,7 +183,7 @@ public class Blur {
                     goutsum += sir[1];
                     boutsum += sir[2];
                 }
-
+                
                 if (i < hm) {
                     yp += w;
                 }
@@ -213,52 +193,52 @@ public class Blur {
             for (y = 0; y < h; y++) {
                 // Preserve alpha channel: ( 0xff000000 & pix[yi] )
                 pix[yi] = (0xff000000 & pix[yi]) | (dv[rsum] << 16) | (dv[gsum] << 8) | dv[bsum];
-
+                
                 rsum -= routsum;
                 gsum -= goutsum;
                 bsum -= boutsum;
-
+                
                 stackstart = stackpointer - radius + div;
                 sir = stack[stackstart % div];
-
+                
                 routsum -= sir[0];
                 goutsum -= sir[1];
                 boutsum -= sir[2];
-
+                
                 if (x == 0) {
                     vmin[y] = Math.min(y + r1, hm) * w;
                 }
                 p = x + vmin[y];
-
+                
                 sir[0] = r[p];
                 sir[1] = g[p];
                 sir[2] = b[p];
-
+                
                 rinsum += sir[0];
                 ginsum += sir[1];
                 binsum += sir[2];
-
+                
                 rsum += rinsum;
                 gsum += ginsum;
                 bsum += binsum;
-
+                
                 stackpointer = (stackpointer + 1) % div;
                 sir = stack[stackpointer];
-
+                
                 routsum += sir[0];
                 goutsum += sir[1];
                 boutsum += sir[2];
-
+                
                 rinsum -= sir[0];
                 ginsum -= sir[1];
                 binsum -= sir[2];
-
+                
                 yi += w;
             }
         }
-
+        
         bitmap.setPixels(pix, 0, w, 0, 0, w, h);
         return (bitmap);
     }
-
+    
 }
--- library/src/com/manuelpeinado/glassactionbar/GlassActionBarHelper.java
+++ library/src/com/manuelpeinado/glassactionbar/GlassActionBarHelper.java
@@ -18,6 +18,7 @@ package com.manuelpeinado.glassactionbar;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.util.TypedValue;
@@ -32,10 +33,32 @@ import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.cyrilmottier.android.translucentactionbar.NotifyingScrollView;
 import com.cyrilmottier.android.translucentactionbar.NotifyingScrollView.OnScrollChangedListener;
 import com.manuelpeinado.glassactionbar.ListViewScrollObserver.OnListViewScrollListener;
+
+
+/**
+ *	This class is modified based on ManuelPeinado's GlassActionBar library (See https://github.com/ManuelPeinado/GlassActionBar).
+ *
+ *	Add reference to list header view so that we can activate blur effect after scrolling to certain point.
+ *
+ *	Basic idea:
+ *	Set background of action bar to null.
+ *
+ *	This helper will put a image view, which is the blur part, and a text view, the action bar title part in the place of
+ *	original action bar.
+ *
+ *	Before we scroll to the boundary of list header, action bar will display as transparent with alpha value changing from 0 to its maximum value.
+ *
+ *	When scroll below boundary, action bar will blur its background. Set background of text view to completely transparent.
+ *
+ *	This helper also applies to scroll view, see NotifyingScrollView.
+ *
+ *	Remember, not to put a list view into scroll view.
+ */
 
 public class GlassActionBarHelper implements OnGlobalLayoutListener, OnScrollChangedListener, BlurTask.Listener, OnListViewScrollListener {
     private int contentLayout;
@@ -47,7 +70,7 @@ public class GlassActionBarHelper implements OnGlobalLayoutListener, OnScrollCha
     private int width;
     private int height;
     private Bitmap scaled;
-    private int blurRadius = GlassActionBar.DEFAULT_BLUR_RADIUS;
+    private int blurRadius = GlassActionBar.MIN_BLUR_RADIUS+1;
     private BlurTask blurTask;
     private int lastScrollPosition = -1;
     private NotifyingScrollView scrollView;
@@ -56,37 +79,77 @@ public class GlassActionBarHelper implements OnGlobalLayoutListener, OnScrollCha
     private static final String TAG = "GlassActionBarHelper";
     private boolean verbose = GlassActionBar.verbose;
     private Drawable windowBackground;
-
+	
+	//Text view of title in action bar
+	private TextView titleView;
+	private int title;
+	private int titleViewBackground = Color.GRAY;
+	
+	//The list header part of ListView
+	private ViewGroup listHeaderView;
+    
     public GlassActionBarHelper contentLayout(int layout) {
         this.contentLayout = layout;
         return this;
     }
-
+    
+    //Pass in list header view for alpha of action bar
+    public GlassActionBarHelper contentLayout(int layout, ViewGroup listHeaderView) {
+    	this.contentLayout = layout;
+    	this.listHeaderView = listHeaderView;
+    	return this;
+    }
+    
     public GlassActionBarHelper contentLayout(int layout, ListAdapter adapter) {
         this.contentLayout = layout;
         this.adapter = adapter;
         return this;
     }
-
+    
+    //Set the header of list view
+    public void setListHeaderView(ViewGroup listHeaderView) {
+    	if (listView.getHeaderViewsCount() == 0) {
+    		listView.addHeaderView(listHeaderView);
+    	}
+    	
+    	this.listHeaderView = listHeaderView;
+    }
+    
+    public void setTitleText(int id) {
+    	title = id;
+    }
+    
+    public void setActionBarBackground(int id) {
+    	titleViewBackground = id;
+    }
+    
     public View createView(Context context) {
         int[] attrs = { android.R.attr.windowBackground };
         
-     // Need to get resource id of style pointed to from actionBarStyle
+        // Need to get resource id of style pointed to from actionBarStyle
         TypedValue outValue = new TypedValue();
         context.getTheme().resolveAttribute(android.R.attr.windowBackground, outValue, true);
         
         TypedArray style = context.getTheme().obtainStyledAttributes(outValue.resourceId, attrs);
         windowBackground = style.getDrawable(0);
         style.recycle();
-            
+        
         LayoutInflater inflater = LayoutInflater.from(context);
         frame = (FrameLayout) inflater.inflate(R.layout.gab__frame, null);
         content = inflater.inflate(contentLayout, (ViewGroup) frame, false);
         frame.addView(content, 0);
-
+        
         frame.getViewTreeObserver().addOnGlobalLayoutListener(this);
         blurredOverlay = (ImageView) frame.findViewById(R.id.blurredOverlay);
-
+        
+        //Initialization of title
+        titleView = (TextView) frame.findViewById(R.id.blurred_action_bar_title);
+        if (title != 0) {
+        	titleView.setText(context.getString(title));
+        }
+        titleView.setBackgroundColor(titleViewBackground);
+        titleView.getBackground().setAlpha(0);
+        
         if (content instanceof NotifyingScrollView) {
             if (verbose) Log.v(TAG, "ScrollView content!");
             scrollView = (NotifyingScrollView) content;
@@ -94,22 +157,29 @@ public class GlassActionBarHelper implements OnGlobalLayoutListener, OnScrollCha
         } else if (content instanceof ListView) {
             if (verbose) Log.v(TAG, "ListView content!");
             listView = (ListView) content;
-            listView.setAdapter(adapter);
             ListViewScrollObserver observer = new ListViewScrollObserver(listView);
             observer.setOnScrollUpAndDownListener(this);
         }
-
+        
         actionBarHeight = getActionBarHeight(context);
         return frame;
     }
-
+    
+    public void setActionBarAlpha(int alpha) {
+    	titleView.getBackground().setAlpha(alpha);
+    }
+    
+    public void setTitle(String title) {
+    	titleView.setText(title);
+    }
+    
     public void invalidate() {
         if (verbose) Log.v(TAG, "invalidate()");
         scaled = null;
         computeBlurOverlay();
         updateBlurOverlay(lastScrollPosition, true);
     }
-
+    
     public void setBlurRadius(int newValue) {
         if (!GlassActionBar.isValidBlurRadius(newValue)) {
             throw new IllegalArgumentException("Invalid blur radius");
@@ -120,11 +190,11 @@ public class GlassActionBarHelper implements OnGlobalLayoutListener, OnScrollCha
         blurRadius = newValue;
         invalidate();
     }
-
+    
     public int getBlurRadius() {
         return blurRadius;
     }
-
+    
     public void setDownsampling(int newValue) {
         if (!GlassActionBar.isValidDownsampling(newValue)) {
             throw new IllegalArgumentException("Invalid downsampling");
@@ -135,17 +205,17 @@ public class GlassActionBarHelper implements OnGlobalLayoutListener, OnScrollCha
         downSampling = newValue;
         invalidate();
     }
-
+    
     public int getDownsampling() {
         return downSampling;
     }
-
+    
     protected int getActionBarHeight(Context context) {
         TypedValue outValue = new TypedValue();
         context.getTheme().resolveAttribute(android.R.attr.actionBarSize, outValue, true);
         return context.getResources().getDimensionPixelSize(outValue.resourceId);
     }
-
+    
     @Override
     public void onGlobalLayout() {
         if (verbose) Log.v(TAG, "onGlobalLayout()");
@@ -153,6 +223,7 @@ public class GlassActionBarHelper implements OnGlobalLayoutListener, OnScrollCha
             if (verbose) Log.v(TAG, "onGlobalLayout() - returning because not first time it's called");
             return;
         }
+        
         int widthMeasureSpec = MeasureSpec.makeMeasureSpec(frame.getWidth(), MeasureSpec.AT_MOST);
         int heightMeasureSpec;
         if (listView != null) {
@@ -162,11 +233,31 @@ public class GlassActionBarHelper implements OnGlobalLayoutListener, OnScrollCha
         }
         content.measure(widthMeasureSpec, heightMeasureSpec);
         width = frame.getWidth();
-        height = content.getMeasuredHeight();
-        lastScrollPosition = scrollView != null ? scrollView.getScrollY() : 0; 
+        height = content.getHeight();
+        
+        if (listView != null) {
+        	height = getTotalHeightofListView();
+            height += listHeaderView == null ? 0 : listHeaderView.getMeasuredHeight();
+        }
+        
+        lastScrollPosition = scrollView != null ? scrollView.getScrollY() : 0;
         invalidate();
     }
-
+    
+    private int getTotalHeightofListView() {
+        
+        ListAdapter mAdapter = listView.getAdapter();
+        int listviewElementsheight = 0;
+        for (int i = 0; i < mAdapter.getCount(); i++) {
+            View mView = mAdapter.getView(i, null, listView);
+            mView.measure(
+                          MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED),
+                          MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+            listviewElementsheight += mView.getMeasuredHeight();
+        }
+        return listviewElementsheight;
+    }
+    
     private void computeBlurOverlay() {
         if (verbose) Log.v(TAG, "computeBlurOverlay()");
         if (scaled != null) {
@@ -180,21 +271,21 @@ public class GlassActionBarHelper implements OnGlobalLayoutListener, OnScrollCha
             if (verbose) Log.v(TAG, "computeBlurOverlay() - scroll position is " + scrollPosition);
         }
         long start = System.nanoTime();
-
+        
         scaled = Utils.drawViewToBitmap(scaled, content, width, height, downSampling, windowBackground);
-
+        
         long delta = System.nanoTime() - start;
         if (verbose) Log.v(TAG, "computeBlurOverlay() - drawing layout to canvas took " + delta/1e6f + " ms");
-
+        
         if (verbose) Log.v(TAG, "computeBlurOverlay() - starting blur task");
         startBlurTask();
-
+        
         if (scrollView != null) {
             if (verbose) Log.v(TAG, "computeBlurOverlay() - restoring scroll from " + scrollView.getScrollY() + " to " + scrollPosition);
             scrollView.scrollTo(0, scrollPosition);
         }
     }
-
+    
     private void startBlurTask() {
         if (verbose) Log.v(TAG, "startBlurTask()");
         if (blurTask != null) {
@@ -203,15 +294,15 @@ public class GlassActionBarHelper implements OnGlobalLayoutListener, OnScrollCha
         }
         blurTask = new BlurTask(frame.getContext(), this, scaled, blurRadius);
     }
-
+    
     private void updateBlurOverlay(int top, boolean force) {
         if (verbose) Log.v(TAG, "updateBlurOverlay() - top=" + top);
-
+        
         if (scaled == null) {
             if (verbose) Log.v(TAG, "updateBlurOverlay() - returning because scaled is null");
             return;
         }
-
+        
         if (top < 0) {
             if (verbose) Log.v(TAG, "updateBlurOverlay() - clamping top to 0");
             top = 0;
@@ -222,7 +313,7 @@ public class GlassActionBarHelper implements OnGlobalLayoutListener, OnScrollCha
         }
         lastScrollPosition = top;
         Bitmap actionBarSection = Bitmap.createBitmap(scaled, 0, top / downSampling, width / downSampling,
-                actionBarHeight / downSampling);
+                                                      actionBarHeight / downSampling);
         // Blur here until background finished (will make smooth jerky during the first second or so).
         Bitmap blurredBitmap;
         if (isBlurTaskFinished()) {
@@ -237,17 +328,17 @@ public class GlassActionBarHelper implements OnGlobalLayoutListener, OnScrollCha
         actionBarSection.recycle();
         blurredOverlay.setImageBitmap(enlarged);
     }
-
+    
     private boolean isBlurTaskFinished() {
         return blurTask == null;
     }
-
+    
     @Override
     public void onScrollChanged(ScrollView who, int l, int t, int oldl, int oldt) {
         // ScrollView scroll
         onNewScroll(t);
     }
-
+    
     @Override
     public void onScrollUpDownChanged(int delta, int scrollPosition, boolean exact) {
         // ListView scroll
@@ -256,12 +347,28 @@ public class GlassActionBarHelper implements OnGlobalLayoutListener, OnScrollCha
             onNewScroll(-scrollPosition);
         }
     }
-
+    
     private void onNewScroll(int t) {
         if (verbose) Log.v(TAG, "onNewScroll() - new scroll position is " + t);
+        
+        if (listHeaderView != null) {
+        	//Use scroll position t instead of listHeaderView.getLocalRect()
+        	//Because when user scrolls fast, listHeaderView.getTop() may return incorrect value
+        	float scrollOffset = (float) Math.abs(listHeaderView.getTop()) / listHeaderView.getHeight();
+        	int alpha = Math.min((int)(200 * scrollOffset), 200);
+        	if (t < listHeaderView.getHeight()) {
+        		//Remove blur effect, set action bar darker
+        		blurredOverlay.setVisibility(View.INVISIBLE);
+            	titleView.getBackground().setAlpha(alpha);
+        	} else {
+        		//titleView.getBackground().setAlpha(0);
+    	        blurredOverlay.setVisibility(View.VISIBLE);
+        	}
+        }
+        
         updateBlurOverlay(t, false);
     }
-
+    
     @Override
     public void onBlurOperationFinished() {
         if (verbose) Log.v(TAG, "onBlurOperationFinished() - blur operation finished");
@@ -269,9 +376,9 @@ public class GlassActionBarHelper implements OnGlobalLayoutListener, OnScrollCha
         updateBlurOverlay(lastScrollPosition, true);
         // Utils.saveToSdCard(scaled, "blurred.png");
     }
-
+    
     @Override
     public void onScrollIdle() {
     }
-
+    
 }
